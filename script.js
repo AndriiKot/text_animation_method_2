9c35a836a5a3c1231b990fec76542857d0b3dda7
AndriiKot <a_kocyuba@ukr.net>
2023-03-06 17:27:14 +0100

    Creation text_animation_method_2

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿const paths = document.querySelectorAll('svg path');
+let step_animation = 0.3;
+let start_animation = 0
+
+for(let i = 0; i < paths.length; i++){
+  let size_element = Math.ceil(paths[i].getTotalLength());
+
+  paths[i].style.strokeDasharray = `${size_element}`;
+  paths[i].style.strokeDashoffset = `${size_element}`;
+  paths[i].style.animation = "anim-line 2s ease forwards " + start_animation + 's';
+  start_animation += step_animation;
+}
+
+const el = document.querySelector('svg')
+el.style.animation = "anim-fill 1s ease forwards " + (start_animation + 2)+ 's';
